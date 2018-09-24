c9b6fb15f813195e63d60859940bc7332774a596
Garrett Hay <garlhay@ut.utm.edu>
2018-09-24 17:34:57 -0500

    Add project files.

---
+++ wpfDatabaseTrial/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfDatabaseTrial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            cn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\\EmployAccessDatabase.accdb");
+        }
+
+        private void See_Assets_Click(object sender, RoutedEventArgs e)
+        {
+            string query = "select * from Assets";
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+            cn.Open();
+            OleDbDataReader read = cmd.ExecuteReader();
+            string data = "";
+            while(read.Read())
+            {
+                data += read[0].ToString() +  " has " + read[1].ToString() + ": " + read[2].ToString() + "\n";
+            }
+            View_Assets.Text = data;
+            cn.Close();
+        }
+
+        OleDbConnection cn;
+
+        private void See_Employees_Click(object sender, RoutedEventArgs e)
+        {
+            string query = "select * from Employees";
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+            cn.Open();
+            OleDbDataReader read = cmd.ExecuteReader();
+            string data = "";
+            while (read.Read())
+            {
+                data += "Employee: " + read[0].ToString() + "\n" + read[1].ToString() + " " + read[2].ToString() + "\n\n";
+            }
+            View_Employees.Text = data;
+            cn.Close();
+        }
+    }
+}
